86fd9cca25054306662fe46345cfbd51a29156db
Ankit Chadoker <ankitchadoker.dev@gmail.com>
2026-02-21 00:43:26 +0530

    iterative approach of quick sort as well as take high as a pivot.

--- quick-sort.js
+++ quick-sort.js
@@ -19,8 +19,8 @@ function partition(arr, low, high){
 	
 	while(i <= j){
 			
-		while(arr[i] <= arr[pivot] && i <= high) i++; 
-		while(arr[j] > arr[pivot] && j >= low) j--;
+		while(arr[i] <= arr[pivot] && i <= j) i++; 
+		while(arr[j] > arr[pivot] && i <= j) j--;
 		
 		if(i <= j){
 			[arr[i], arr[j]] = [arr[j], arr[i]];
@@ -32,16 +32,47 @@ function partition(arr, low, high){
 	return j;
 }
 
+function partitionAsHigh(arr, low, high){
+	let pivot = high;
+	let j = high - 1;
+	let i = low;
+	
+	while(i <= j){
+		while(i <= j && arr[i] <= arr[pivot]) i++;
+		while(j >= i && arr[j] > arr[pivot]) j--;
+		if(i <= j){ [arr[i], arr[j]] = [arr[j], arr[i]]; i++; j--;}
+	}
+	[arr[pivot], arr[i]] = [arr[i], arr[pivot]];
+	return i;
+}
+
 function quickSort(arr, low = 0, high = arr.length - 1){
 	if(low < high){		
-		//let pIndex = partition(arr, low, high);
+		let pIndex = partition(arr, low, high);
 		
 		//** randomizing the pivot would help us tackle the worst case time complexity from O(n^2) to O(n*logn), for sorted array, roteted sorted array or partial sorted arrays **/
-		let pIndex = randomPartition(arr, low, high);
+		//let pIndex = randomPartition(arr, low, high);
 		
 		quickSort(arr, low, pIndex-1);
 		quickSort(arr, pIndex+1, high);
 	}
 	return arr;
 }
-console.log(quickSort([3,5,4,1,2]));
+//console.log(quickSort([3,4,2,1,5]));
+
+
+function quickSortIterative(arr){
+	const stack = [];
+	stack.push([0, arr.length - 1]);
+	while(stack.length > 0){
+		const [low, high] = stack.pop();
+		if(low < high){
+			let pIndex = randomPartition(arr, low, high);
+			stack.push([low, pIndex-1]);
+			stack.push([pIndex + 1 , high]);
+		}
+	}
+	return arr;
+}
+
+//console.log(quickSortIterative([1,2,2,4,5,1,4,54,645,3,5,4]));
