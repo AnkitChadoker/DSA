61423b73c1f1fd3345fc6d3034231a37d31a6f33
Ankit Chadoker <ankitchadoker.dev@gmail.com>
2026-02-20 23:37:31 +0530

    quick sort with random partitioning for optimized TC.

--- quick-sort.js
+++ quick-sort.js
@@ -1,3 +1,14 @@
+function randomPartition(arr, low, high) {
+		//** suppose we have high = 4, low = 3 **/
+		const randomIndex = Math.floor(Math.random() * (high - low + 1)) + low;
+		//** Math.random() will give as any float value 0 <= value < 1
+		// ** (high - low + 1) will give us the size of the elements (4-3+1) = 2 array of [1,2] from taken array
+		//** Math.floor(Math.random() * 2) => will always give us the number between 0 (inclusive) to 2 (exlusive) because floor will round down the value so there will be only 2 values possible for 2 size aarray which is 0 and 1.
+		//** but our array indexing is begining from 3 (low) so we shift the above calculation by low, thats why we added +low at the end to shift the indexing to the desired window (3 to 4). **//
+		[arr[low], arr[randomIndex]] = [arr[randomIndex], arr[low]];
+		return partition(arr, low, high);
+}
+
 function partition(arr, low, high){
 	const pivot = low;
 	let i = low + 1;
@@ -19,11 +30,15 @@ function partition(arr, low, high){
 }
 
 function quickSort(arr, low = 0, high = arr.length - 1){
-	if(low < high){
-		let pIndex = partition(arr, low, high);
+	if(low < high){		
+		//let pIndex = partition(arr, low, high);
+		
+		//** randomizing the pivot would help us tackle the worst case time complexity from O(n^2) to O(n*logn), for sorted array, roteted sorted array or partial sorted arrays **/
+		let pIndex = randomPartition(arr, low, high);
+		
 		quickSort(arr, low, pIndex-1);
 		quickSort(arr, pIndex+1, high);
 	}
 	return arr;
 }
-console.log(quickSort([1,2,3,4,5,6]));
+console.log(quickSort([3,5,4,1,2]));
