49a30892916e10a12c52bf4c6be7732bda345d77
Ankit Chadoker <ankitchadoker.dev@gmail.com>
2026-02-22 01:31:01 +0530

    remove duplicate from sorted array optimal approach.

--- array/easy/remove-duplicates-from-sorted-array.js
+++ array/easy/remove-duplicates-from-sorted-array.js
@@ -25,4 +25,28 @@ function removeDuplicateFromSortedArray(arr){
 	//** we are using an extra space to store the unique elements and in the worst case when there is no duplicate in the array the SC will be O(n) as well.
 }
 
-console.log(removeDeuplicateFromSortedArray([1,1,2,2,2,3,3,3,4]))
+//console.log(removeDuplicateFromSortedArray([1,1,2,2,2,3,3,3,4]))
+
+//** optimal approach **//
+
+//** since the array is already sorted so we can confirm that is the next element is either gonna be the same element or bigger element, so we can take 2 pointer (i, j) if we encounter the same element we can move the (j) pointer to find the bigger element and replace the (i) pointer with the (j) pointer value
+//** since we only need the length of unique array we do not care what ever be stored after that be it null, undefined until explicitly asked.
+//** arr = [1,1,2,2,2,3,3,3,4] => [1,2,3,4,_,_,_,_,_]
+//** once pointer j crossed the array length the pointer i will be the length of unique array.
+
+//** suppose we put the pointer i at the 0th index becuase the 0th index will always be gonna included in the unique array and we put the j pointer to the 1st index and start the iteration like is j equal to i if yes we march forward (j++) again check is j equal to i this time its no so we swap the value of j with the i+1 (next) index and also do i++ to keep the track of array uniqueness till that index. we repeat the process until j crosses the array at that point what ever the value of i will be out answer (i+1, becuase i is representing the index and we want the length)
+
+function optimalRemoveDuplicateFromSortedArray(arr){
+	i = 0;
+	for(let j = 1; j < arr.length; j++){
+		if(arr[i] !== arr[j]){
+			arr[i+1] = arr[j];
+			i++;
+		}
+	}
+	return i+1;
+	
+	//** since we are only going through the array once so TC O(n).
+	//** no extra space is being used in-place removal of the duplicate array is being done so SC O(1)
+}
+console.log(optimalRemoveDuplicateFromSortedArray([1,2,3,4,5]))
