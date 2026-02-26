5bac5b0a662aa33da547047900d85ea0bca334d1
Ankit Chadoker <ankitchadoker.dev@gmail.com>
2026-02-27 00:14:15 +0530

    optimal intersection of sorted array.

--- array/easy/intersection-of-array.js
+++ array/easy/intersection-of-array.js
@@ -1,5 +1,5 @@
 /** 
-we are given two arrays and need to return the new array containing intersection of these two array.
+we are given two sorted arrays and need to return the new array containing intersection of these two array.
 The intersection of two arrays is an array where all values are common and are present in both the arrays.
 
 arr1 = [1,2,3,4,5,6]
@@ -8,13 +8,16 @@ output = [1,5,6]
 
 **/
 
+/** BRUTE FORCE **/
 function intersectionOfArray(arr1, arr2){
 	const intersection = [];
 	
 	for(let i = 0; i < arr1.length; i++){
 		for(let j = 0; j < arr2.length; j++){
 			if(arr1[i] === arr2[j]){
-				intersection.push(arr1[i]); break;
+				if(intersection[intersection.length - 1] !== arr1[i]){									
+					intersection.push(arr1[i]); break;
+				}
 			}
 		}
 	}
@@ -23,10 +26,43 @@ function intersectionOfArray(arr1, arr2){
 	
 	/**
 		TC: we are looping through both the arrays entirely though we are using the break statement on the match but there could be the posibility that no element are common in the arrays, thats the worst case posible so,
-		O(m*n)
+		O(m * n)
 		
-		SC: O(min(m,n)) // there can not be more elements in the intersection arary then the min. of these two array element, because we are checking for common elements and it should be present in both the arrays
+		SC: O(min(m,n)) // there can not be more elements in the intersection arary then the min. of these two arrays, because we are checking for common elements and it should be present in both the arrays
 	**/
 }
 
-console.log(intersectionOfArray([1,2,3,4,5,6], [7,8,9]));
+//console.log(intersectionOfArray([1,2,3,4,5,6], [7,8,9]));
+
+
+/** OPTIMAL APPROACH **/
+function optimalIntersection(arr1, arr2){
+	const intersection = [];
+	let i = 0;
+	let j = 0;
+	while(i < arr1.length && j < arr2.length){
+		if(arr1[i] === arr2[j]){
+			if(intersection[intersection.length - 1] !== arr1[i]){
+				intersection.push(arr1[i]);
+			}
+			i++;
+			j++
+		} else {
+			if(arr1[i] < arr2[j]){
+				i++;
+			} else {
+				j++
+			}
+		}
+	}
+	
+	return intersection;
+	
+	/**
+		TC: O(max(n,m)) // at most we can loop the max. of any of these to array, which is linear TC.
+		SC: O(min(n,m))
+	**/
+}
+
+console.log(optimalIntersection([1,2,2,2,2,2,2,3,4,5,6], [1,5,5,5,5,5,6,7,8,9]));
+
