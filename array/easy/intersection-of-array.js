05e7adf4b5ce9b8268f8f3a9874c0f3f94857714
Ankit Chadoker <ankitchadoker.dev@gmail.com>
2026-02-27 00:48:44 +0530

    better than brute force approach as well for intersection of 2 sorted arrays.

--- array/easy/intersection-of-array.js
+++ array/easy/intersection-of-array.js
@@ -31,8 +31,45 @@ function intersectionOfArray(arr1, arr2){
 		SC: O(min(m,n)) // there can not be more elements in the intersection arary then the min. of these two arrays, because we are checking for common elements and it should be present in both the arrays
 	**/
 }
+//console.log(intersectionOfArray([1,1,1,1,1,2,2,2,2,3], [1,1,1,1,3,3,3]));
 
-//console.log(intersectionOfArray([1,2,3,4,5,6], [7,8,9]));
+/** BETTER APPROACH **/
+function betterIntersectionOfArray(arr1, arr2){
+	const intersectionMap = new Map();
+	
+	/*** put all the elements of first array into the map with frequecy of one only becuase there can be repeated elements as well in the same array so we need to stick to the frequecy 1 only, because at the end we are checking if any elemt has frequecy more than 1 those are our guys. **/
+	
+	for(let i =0; i < arr1.length; i++){
+		if(!intersectionMap.has(arr1[i])){
+			intersectionMap.set(arr1[i], 1);
+		}
+	}
+	
+	
+	/*** we only care about the matched element from the map which was set from array 1 and we increse its frequecy if its already not more than 2, becausewe only care if the elementis appearing in both the array so we increase its frequecy till 2 only. **/
+	
+	for(let i =0; i < arr2.length; i++){
+		if(intersectionMap.has(arr2[i]) && intersectionMap.get(arr2[i]) < 2){
+			intersectionMap.set(arr2[i], intersectionMap.get(arr2[i]) + 1);
+		}
+	}
+	
+	const intersection = [];
+	for(let [element, frequency] of intersectionMap){
+		if(frequency > 1){ intersection.push(element)}
+	}
+	
+	return intersection;
+	
+	/**
+		TC: O(2(M + N)) // we are looping the arrays and then again the map as well of the same size (at worst).
+		SC: O(min(M,N)) + O (M + N) // min becuase the minimum elements will be stored in the intersection array, and O(M+N) for the map (at worst).
+		
+		so, we brought down the TC in expense of some extra O(M+N) space compare to brute force approach.
+	**/
+}
+
+console.log(betterIntersectionOfArray([1,1,1,1,1,2,2,2,2,3,5,6,8,8], [1,1,1,1,3,3,3,4,4,4,7,7,7,8]));
 
 
 /** OPTIMAL APPROACH **/
@@ -64,5 +101,5 @@ function optimalIntersection(arr1, arr2){
 	**/
 }
 
-console.log(optimalIntersection([1,2,2,2,2,2,2,3,4,5,6], [1,5,5,5,5,5,6,7,8,9]));
+//console.log(optimalIntersection([1,1,1,1,1,2,2,2,2,3], [1,1,1,1,3,3,3]));
 
