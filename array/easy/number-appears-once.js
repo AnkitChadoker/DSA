9fe09c0931d8a42f3978c5e7d23d91fdb57a3311
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-03-01 02:23:59 +0530

    find the number appearing only once in the array, OPTIMAL APPRAOCH as well as complementry approach as well using set.

--- array/easy/number-appears-once.js
+++ array/easy/number-appears-once.js
@@ -48,9 +48,67 @@ function betterOnceAppearedNumber(arr){
 	}	
 
 	/** 
-	  	TC: O(n) for iterating over array once to record the frequency of the elements, another O(n) for preparing the response while iterating over map (O(n + n)).
-	  	SC: O(n), map space.
-	** /
+	  	TC: O(n) for iterating over array once to record the frequency of the elements, another O((n/2)+1) for preparing the response while iterating over map (O(n + (n/2)+1), because map will have n/2 + 1 elements at max because all the elements are repeating except for one.
+	  	SC: O(n/2 + 1), because all the elements will be appearing twice except the one element so map will have (n/2+1) elements.
+	**/
 }
 
-console.log(betterOnceAppearedNumber([1,4,4,1,2])); // 2
+//console.log(betterOnceAppearedNumber([1,4,4,1,2])); // 2
+
+/** OPTIMAL APPROACH **/
+/** we can use XOR OPERATOR here again because problem itself stats that all the elements would be appearing twice which will cancel themself out except for the one which is appearing once. **/
+
+function optimalOnceAppearedNumber(arr){
+	let xor = 0;
+	for(let i = 0; i < arr.length; i++){
+		xor ^= arr[i];
+	}
+
+	return xor;
+
+	/*
+		TC: O(n)
+		SC: O(1)
+	*/
+}
+//console.log(optimalOnceAppearedNumber([1,2,4,1,2])); // 4
+
+
+//** Complimantry solution, using array sum **/
+
+function complemantryOnceAppearedNumber(arr){
+	//** get all the unique elements from the array first. **/
+	const set = new Set()
+	let sumOfArr = 0;
+
+	for(let i = 0; i< arr.length; i++){
+		set.add(arr[i]);
+		sumOfArr += arr[i];
+	}
+
+	let sumOfSet = 0;
+	for(const value of set){
+		sumOfSet += value;
+	}
+
+	return 2*sumOfSet - sumOfArr;
+
+	/*
+		TC: O(n + (n/2 +1))
+		SC: O(n/2 +1)
+
+		WHY THIS WORKED: since array would have all the element twice (multiple of 2) except for one element, so if we get all the unique elements from the array (using set, [1,2,4]) and if we sum and double it (2 * sum) and then minus the sum of the array elements ( all the elements will be double execpt for one, and when we minus it from the set sum we will get that single element) we will get the desired element 
+
+		set = [1,2,4] => 2 * 7 = 14
+		array = [1,4,4,2,2] => 13
+
+		14 - 13 = 1 (output)
+	*/
+}
+
+console.log(complemantryOnceAppearedNumber([1,4,4,2,2])); // 1
+
+
+
+
+
