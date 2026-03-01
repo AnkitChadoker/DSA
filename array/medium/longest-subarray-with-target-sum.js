c8a0ecf9f23fcec1f0a73eef4bf97d66956fd2ec
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-03-01 17:02:34 +0530

    better BRUTE force approach to shrink down TC to O(n^2) from O(n^3).

--- array/medium/longest-subarray-with-target-sum.js
+++ array/medium/longest-subarray-with-target-sum.js
@@ -13,6 +13,7 @@
  **/
 
 /** BRUTE FORCE **/
+
 /** try out all posible sub arrays **/
 /** like we know 
   	i = 0 (sub arrays started from i = 0) =>	[10], [10,5], [10,5,2], [10,5,2,7], [10,5,2,7,1]
@@ -28,15 +29,15 @@
 function longestSubArrayWithTargetSum(arr, target){
 	let longestSubArrayLength = 0;
 	//** starting point of new subsets each time **/
-	for(let i = 0; i < arr.length; i++){
+	for(let i = 0; i < arr.length; i++){ //TC: O(n)
 
 		/** from where the subsets will begin to make **/
-		for(let j = i; j < arr.length; j++){
+		for(let j = i; j < arr.length; j++){ //TC: O(n) approax (each time there will be increased i)
 
 			/** create subsets starting from i till j, like [10] (when i  = 0, j = 0), [10,5] (when i = 0, j = 1) and so on **/
 			let subArraySum = 0;
 			let subArrayLenght = 0;
-			for(let k = i; k <= j; k++){
+			for(let k = i; k <= j; k++){ //TC: O(n) approax (at worst case when i at 0 and j at arr.length - 1, we have the biggest subarray (entire array))
 
 				subArrayLenght += 1;
 				subArraySum += arr[k];
@@ -55,6 +56,39 @@ function longestSubArrayWithTargetSum(arr, target){
 	}
 
 	return longestSubArrayLength;
+
+	/**
+	 	TC: O(n^3) // approax
+	 	SC: O(1) 
+	 */
 }
 
-console.log(longestSubArrayWithTargetSum([10, 5, 2, 7, 1, 9], 15));
+//console.log(longestSubArrayWithTargetSum([10, 5, 2, 7, 1, 9], 15));
+
+
+/** Better BRUTE FORCE **/
+function BBFlongestSubArrayWithTargetSum(arr, target){
+	let longestSubArrayLength = 0;
+
+	for(let i = 0; i < arr.length; i++){
+		subArraySum = 0
+		for(let j = i; j < arr.length; j++){
+			/** 
+			 previously we were using extra loop to iterate from i to j but if we carefully observe we are just adding the current j th element into the sum becuase lets say i = 1, j = 4
+			 so we already had the sum of i = 1, j = 3 // [5, 2, 7] we just need to add 1 (j = 4) into it only.
+			 in each iteration only j is getting incremented i is on 1 only so we can add j[0] + j[1] + j[2] + j[3] and so on,
+			 these will reduce the TC to approax O(n^2)
+			*/
+			subArraySum += arr[j];
+			if(subArraySum === target) {
+				longestSubArrayLength = Math.max(longestSubArrayLength, j-i+1);
+			} else if(subArraySum > target) {
+				break;
+			}
+		}
+	}
+
+	return longestSubArrayLength;
+}
+
+console.log(BBFlongestSubArrayWithTargetSum([10, 5, 2, 7, 1, 9], 15));
