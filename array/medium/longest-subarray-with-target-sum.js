e03a59b7bd4b172e72d279a7957a2ec9f7a9caa7
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-03-02 12:50:59 +0530

    optimal and better solution for finding longest sub array for the target sum.

--- array/medium/longest-subarray-with-target-sum.js
+++ array/medium/longest-subarray-with-target-sum.js
@@ -96,8 +96,6 @@ function BBFlongestSubArrayWithTargetSum(arr, target){
 
 /** BETTER SOLUTION **/
 
-/*** THIS IS FOR ONLY NUMBER ABOVE 0 **/
-
 /** we can use map and store the index and the sum at that index and on each iteration if we match the target thats our new lenght if its obviously greater than previous lenght, and if its greater than the target we can check if at any index we have the value which is the difference of (current sum - target), because from that index onwards the sum of all the elements till the current "i" is equal to target.
 	 
   i   sum
@@ -130,12 +128,90 @@ function betterLongestSubArrayWithTargetSum(arr, target){
 			longest = Math.max(longest, i - map.get(diff));
 		}
 
-		map.set(sum, i);
+		/** 
+		 	in case of 0 and negative numbers we need to handle this below edge case with condition, becuase if add 0 anything in it will be the same sum but the indexing will be updated for that sum in the map so it give us the smallest sub array intead of longest, try to understand it using [2,0,0,3] array.
+
+		 	 sum index
+		 	|         |
+		 	| 5		3 | greater than target by 2 so we check for the 2 in previous indexes where sum was 2 at any point and its at 
+		 				index 2 instead it should have been at index 0 becuase we need longest sub array so we want it be as left as posible.
+
+		 	| 2		2 |	this will not be added as new entries this will update the map where sum is 2 it will change the index 0 to 1 
+		 				to 2
+		 	| 2		1 |
+		 	| 2		0 |
+		 	___________
+
+		 	so now for index 3 it will return the index 2 as previous count existed where difference was 2, ideally it should have been 0, that why we need to add extra check if the sum already exists in the map do not update it.
+
+		 	and this will also work for negative number as well, and it will be OPTIMAL SOLUTION for the array containing 0 and negative elements as well
+		*/
+		if(!map.has(sum)){
+			map.set(sum, i);
+		}
 		
 	}
 	return longest;
+
+	/*
+		TC: O(n)
+		SC: O(n)
+	*/
 }
 
-console.log(betterLongestSubArrayWithTargetSum([10, 5, 2, 7, 1, 9, 5, 1, 1, 1, 1, 1, 1, 1, 1, 2], 15)); // 10
+//console.log(betterLongestSubArrayWithTargetSum([2,0,1,1,1,0,3], 3)); // 5
+
+
+/** OPTIMAL SOLUTION **/
+/*** THIS IS FOR ONLY NUMBER ABOVE 0 (POSITIVE NUMBERS) **/
+
+/** we can use 2 pointers approach here pointer i to denote starting of the subarray and pointer j to denote end of the subarray, we keep incrementing pointer j until we go past (more than) the target value, once we go past the target we stop incrementing j pointer instead we shrink down the sub array from start meaning incremenenting i pointer until the sum becomes less then target, this way we will cover the all posible subarrays as well and will be able to find the subarray matching the target as well, between this pointer increment and decrement we will ofcource be checking for the target matching the sum as well. */
+
+function optimalLongestSubArrayWithTargetSum(arr, target){
+	let start = 0;
+	let end = 0;
+	let longest = 0;
+	let sum = arr[0]; // initially sum will be the first element of array iteself, becuase both start and end are at 0th index so this will be our first subarray itself.
+
+	/* at any point start crosess the end meaning we do not have the target value in the array itself. */
+	while(start <= end && end < arr.length){
+
+		/** as discussed above if sum is greater than target we shrink the sub array from start, but first try to dry run the code below this while than it will be more clear. **/
+
+		while(sum > target){			
+			sum -= arr[start];
+			start++;
+			if(target === sum ){
+				longest = Math.max(longest, end-start+1);
+			}			
+		}
+
+		end++;
+		
+		/** if end at (arr.length - 1) index, then without checking the condition it will cross the bound of array after increment (++) **/
+		if(end < arr.length){			
+			sum += arr[end];
+			if(target === sum){
+				longest = Math.max(longest, end-start+1);
+			}
+		}
+	}
+
+	return longest;
+
+	/** 
+	 	* if we observe carefully wwe are doing end++ and the calulating the sum while we are calculating the sum and doing start++, because in case of start we must minus the current value of start first then only we can move shrink the subarray while at time of end we need to first inlarge the sub array and then we can calculate the sum
+	 	
+	 	TC: 
+	 		* first loop is definately running from 0 to n-1 so clearly its O(n).
+	 		* here is the most confusing part with the naked eye it looks like O(n^2) becuase of nested loop structure, but it is not running for n time for each iteration of outer loop, it is entirely dependent on the condition which may or may not be true for each iteration of outer loop so it can run sometimes for any iteration of outer loop or it may not run at all or any iteration, so it will be additional O(n) not multiplication but addition.
+	 		* total TC: O(2n).
+	 	SC: O(1)
+
+	 	NOTE: THIS ALGO. IS ONLY FOR THE ARRAY CONTAINING ELEMENTS BIGGER THAN 0, for arrays containing negative and 0 "better" approach is optimal for that problem.
+	**/
+}
+
+console.log(optimalLongestSubArrayWithTargetSum([2,1,1,1,3], 15)); // 3
 
 
