615a4f8ae34f17403e387f199141ad6ea23c06c2
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-03-08 00:34:05 +0530

    longest consecutive sequence in array, BETTER APPROACH.

--- array/medium/longest-consecutive-sequence.js
+++ array/medium/longest-consecutive-sequence.js
@@ -20,7 +20,7 @@ function longestConsecutiveSequence(arr){
 
 	for(let i = 0; i < arr.length; i++){
 		
-		let length = 1;
+		let length = 1; // because regardless of consecutive sequence even if there is one element the sequence length will be 1 always, arr[i] th element.
 		let nextEle = arr[i] + 1;
 
 		while(search(nextEle, arr)){
@@ -46,4 +46,35 @@ function search(needle, haystack){
 	return false;
 }
 
-console.log(longestConsecutiveSequence([100, 4, 200, 1, 3, 2, 101, 102, 103, 104]));
+//console.log(longestConsecutiveSequence([100, 4, 200, 1, 3, 2, 101, 102, 103, 104]));
+
+/** BETTER APPROACH **/
+/** If interviewer allow to modify the given array we can sort the array and then linearly can check for the consecutive sequences **/
+
+function betterLongestConsecutiveSequence(arr){
+	arr.sort((a,b) => a-b);
+
+	let longest = 0;
+	let currentSequence = 1; // because regardless of consecutive sequence even if there is one element the sequence length will be 1 always, arr[i] th element.
+
+	for(let i = 0; i < arr.length; i++){
+		if(arr[i+1] === arr[i] + 1){
+			currentSequence++;
+		} else if(arr[i+1] === arr[i]){
+			/** for  understanding purpose kept this block otherwise we can directly check for elseif(arr[i+1] !== arr[i]) and then do currentSequence = 1 **/
+			// do nothing
+		} else {
+			currentSequence = 1;
+		}
+
+		longest = Math.max(currentSequence, longest);
+	}
+
+	return longest;
+
+	/** 
+	 * TC: O(n* logn) + O(n) /// sorting + linear scanning
+	 * SC: O(1)
+	 * **/
+}
+console.log(betterLongestConsecutiveSequence([100, 4, 200, 1, 3, 2, 101, 102,1, 2, 2, 2, 4, 4, 4])); // 4 [1,2,3,4]
