dc0f7a082467d1d4c619fe68e14e23ca98152bdf
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-03-16 00:18:15 +0530

    majority element II, BETTER APPROACH.

--- array/hard/majority-element-II.js
+++ array/hard/majority-element-II.js
@@ -5,7 +5,7 @@
 **/
 
 
-/** BRUTE FORCE **/
+												/** BRUTE FORCE **/
 /** we can simply iterate over the array and check for each element linearly if the element is appearing more than n/3 times and and we can push the element into the result array. **/
 
 /* NOTE: from the majority element I problem we knew that there can be only 1 posible element which can apprear more then half the size of array, similarly here there can at most only 2 elements are possible becuase lets say we have a array size 10 and 10/3 would be 3 (floor value) so there can only be 2 element at max which can appear more than 3 times (min 4 times), beacuse (4+4 = 8) and there would be not much room for any other element to appear more than 3 times.  */
@@ -35,4 +35,70 @@ function bruteMajorityElementII(arr){
 	**/
 } 
 
-console.log(bruteMajorityElementII([1,2,1,1,3,2,2])); // [1,2]
+//console.log(bruteMajorityElementII([1,2,1,1,3,2,2])); // [1,2]
+
+
+												
+
+
+												/** BETTER APPROACH **/
+/** instead of iterating twice over the same array we can store the frequncy of the elements in the map and then can return the elements whose frequncy are greater than majority **/
+
+function betterMajorityElementII(arr){
+	const majority = Math.floor(arr.length/3);
+	const map = new Map();
+
+	for(let i = 0; i < arr.length; i++){
+		if(map.has(arr[i])){
+			map.set(arr[i], map.get(arr[i]) + 1);
+		} else {
+			map.set(arr[i], 1)
+		}
+	}
+
+	const answer = [];
+	for(const [ele, freq] of map){
+		if(freq > majority){
+			answer.push(ele);
+			if(answer.length === 2) break;
+		}
+	}
+
+	return answer;
+
+	/** 
+	 * TC: O(n) + O(n) = O(2n)
+	 * SC: O(n)
+	**/
+}
+
+//console.log(betterMajorityElementII([1,2,1,1,3,2,2])); // [1,2]
+
+/** we can save the extra looping over the map by just checking at the time of setting the value in the map if the element has frequency more than the majority we can directly add it into the result array. **/
+
+function betterOptimalMajorityElementII(arr){
+	const majority = Math.floor(arr.length/3);
+	const answer = [];
+	const map = new Map();
+
+	for(let i = 0; i < arr.length; i++){
+		if(map.has(arr[i])){
+			const freq = map.get(arr[i]) + 1;
+			if(freq > majority){
+				answer.push(arr[i]);
+				if(answer.length === 2) break;
+			}
+		 	map.set(arr[i], freq);
+		} else {
+			map.set(arr[i], 1);
+		}
+	}
+
+	return answer;
+
+	/**
+	 * TC: O(n)
+	 * SC: O(n)
+	**/
+}
+console.log(betterOptimalMajorityElementII([1,2,1,1,3,2])); // [1,2]
