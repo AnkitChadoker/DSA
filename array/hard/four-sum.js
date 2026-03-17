66708d8818b8ff2d76e534b808fce9276a0351e2
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-03-17 23:14:37 +0530

    Four sum, BETTER.

--- array/hard/four-sum.js
+++ array/hard/four-sum.js
@@ -43,6 +43,41 @@ function fourSum(arr, target){
 	**/ 
 }
 
-console.log(fourSum([1, 1, 3, 4, -3], 5))
+//console.log(fourSum([1, 1, 3, 4, -3], 5))
 
 /** BETTER SOLUTION **/
+/** again similar to three sum, in three sum we were keeping i as static pointer here we are keeping i and j both as static pointer and using k to fing the l. **/
+
+function betterFourSum(arr, target){
+	const uniqueSet = new Set();
+	const result = [];
+
+	for(let i = 0; i < arr.length; i++){
+		for(let j = i +1; j < arr.length; j++){			
+			const hashSet = new Set();
+			for(let k = j + 1; k < arr.length; k++){
+				const l = target - (arr[i] + arr[j] + arr[k]);
+				if(hashSet.has(l)){
+					const temp = [arr[i], arr[j], arr[k], l];
+					temp.sort((a,b) => a - b);
+					const key = temp.join(',');
+					if(!uniqueSet.has(key)){
+						result.push(temp);
+						uniqueSet.add(key);
+					}
+				}
+				hashSet.add(arr[k])
+			}
+
+		}
+	}
+	return result;
+
+	/**
+	 * TC: O(n^3)
+	 * SC: O(2* number of quadruplets) + O(n)
+	**/ 
+}
+
+
+console.log(betterFourSum([1,0,-1,0,-2,2], 0)); //[ [-1,0,0,1], [ -2,-1,1,2], [-2,0,0,2]]
