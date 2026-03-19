a56bb4e857d62637844639dee65b7cd1beda70fc
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-03-19 16:24:23 +0530

    longest sub array with sum 0, BRUTE.

---
+++ array/hard/longest-subarray-with-sum-0.js
@@ -0,0 +1,40 @@
+/**
+ * We are given an array of integers and we need to find the length of the lonegest subarray with the sum 0.
+ * 
+ * arr = [-2, -1, 0, 1, 2]
+ * output = 5
+ * 
+**/
+
+													/** BRUTE FORCE **/
+/* like all other sub array problem we can generate all posible sub arrays and check for the given constraints */
+
+function LSAwith0(arr){
+	let maxLen = 0;
+
+	/** starting of sub array **/
+	for(let i = 0; i < arr.length; i++){
+
+		/** generating the sub array **/
+		for(let j = i; j < arr.length; j++){
+			let sum = 0;
+
+			/** iterating over the sub array to sum the elements **/
+			for(let k = i; k <= j; k++){
+				sum += arr[k];
+				if(sum === 0){
+					maxLen = Math.max(maxLen, k - i + 1);
+				}
+			}
+		}
+	}
+
+	return maxLen;
+
+	/**
+	 * TC: near about O(n^3)
+	 * SC: O(1)
+	**/
+}
+
+console.log(LSAwith0([1, -1, 0, -2, -1, 0, 2, 1, 1])); //8
