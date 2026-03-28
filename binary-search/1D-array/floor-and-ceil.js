39ef291bf962d4f76964f6e0b7ef046746a49782
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-03-28 19:24:39 +0530

    Find floor and ceil of the given element in the sorted array, OPTIMAL.

--- binary-search/1D-array/floor-and-ceil.js
+++ binary-search/1D-array/floor-and-ceil.js
@@ -19,7 +19,7 @@
 
 
 
-/** BRUTE FORCE **/
+														/** BRUTE FORCE **/
 
 /** we can simply iterate over the array linearly and can check if any where we find the exact match that is our floor and ceil both otherwise we look for the element which is greatest small element than the x (floor ele.), and greatest small ele than the x (ceil) as soon as we have both we can return them other wise at the end return accordingly **/
 
@@ -45,6 +45,31 @@ function findFloorAndCeil(arr, x){
 	**/
 }
 
-console.log(findFloorAndCeil([3, 4, 4, 7, 8, 10], 5));
+//console.log(findFloorAndCeil([3, 4, 4, 7, 8, 10], 5));
+
+															//** OPTIMAL APPROACH **/
+
+/** we can use binary search and if any moment we got our mid equal to x we can return from there directly otherwise keep on looking for the ceil and floor value like in brute force way. **/
+
+function optimalFindFloorAndCeil(arr, x){
+	let floor = undefined; let ceil = undefined;
+	let left = 0; let right = arr.length - 1;
+
+	while(left <= right){
+		const mid = Math.floor((left + right) / 2);
+
+		if(arr[mid] === x) return [x, x];
+		else if(arr[mid] < x) {
+			floor = floor === undefined ? arr[mid] : Math.max(floor, arr[mid]);
+			left = mid + 1;
+		} else {
+			ceil = ceil === undefined ? arr[mid] : Math.min(ceil, arr[mid]);
+			right = mid - 1;
+		}
+	}
+
+	return [floor || -1, ceil || -1];
+}
 
 
+console.log(optimalFindFloorAndCeil([2, 4, 6, 8, 10, 12, 14], 1));
