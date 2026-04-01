cb158e74a29c0f7195a6973e616aee82ce778efb
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-04-02 00:41:53 +0530

    find single element in the sorted array, OPTIMAL.

--- binary-search/1D-array/single-element-in-sorted-array.js
+++ binary-search/1D-array/single-element-in-sorted-array.js
@@ -61,4 +61,45 @@ function fundamentalSingleNonDuplicate(arr){
 	 * SC: O(1)
 	**/
 }
-console.log(fundamentalSingleNonDuplicate([1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6])); // 4
+//console.log(fundamentalSingleNonDuplicate([1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6])); // 4
+
+
+/** OPTIMAL APPROACH **/
+/**
+ * Our fundamental approach is gonna help us here as well for the edge cases and for the remaining part we need know that the array is sorted and we need to find something in that array so we can basically use the binary search algo. and we know the basic concept of binary search is to eliminate the half array at each iteration, so to do that we need to observe some pattern here, like we need to play around with the indices,
+ * 
+ * 	indices = [0 1 2 3 4 5 6 7 8 9 10]
+ * 	arr =     [1 1 2 2 3 3 4 5 5 6 6] 
+ * 
+ * we know that our target element is 4 so if we look at the left side of the target element and see the indices of array, its following the even-odd pattern (meaning each pair has the indexing as even-odd pattern like (0,1) (2,3) (4,5)) and at the right side of the array we have the index mapping as odd-even pattern, meaning if we are at any index and if it is following the even-odd pattern meaning target element would be on the right side only so we eliminate the left half otherwise we eliminate the right half and for each mid can check if it has no matching neighbors if this condition satisfies we got our target.
+**/
+function optimalSingleNonDuplicate(arr){
+	if(arr.length === 1) return arr[0];
+	if(arr[0] !== arr[1]) return arr[0];
+	if(arr[arr.length - 1] !== arr[arr.length - 2]) return arr[arr.length - 1];
+
+	let low = 1; let high = arr.length - 2;
+
+	while(low <= high){
+		const mid = Math.floor((low + high) / 2);
+		if(arr[mid - 1] !== arr[mid] && arr[mid] !== arr[mid + 1]) return arr[mid];
+
+		/** checking for even-odd pattern, if current index is at even index we check the next element if it's also the same to follow the (e-o pattern) and if the current index is at odd pattern we check the previous element if it's the same (e-o pattern) **/
+		if((mid % 2 === 0 && arr[mid] === arr[mid + 1]) || (mid % 2 === 1 && arr[mid - 1] === arr[mid])){
+
+			/** if pattern follows we will not the element in the left half so we move low to mid + 1 **/
+			low = mid + 1;
+		} else {
+			
+			/** if the e-o pattern is not being followed our target should be at the left half thats why the pattern matching is disrupted. **/
+			high = mid - 1;
+		}
+	}
+
+	return -1;
+	/**
+	 * TC: O(log n)
+	 * SC: O(1)
+	**/
+}
+console.log(optimalSingleNonDuplicate([1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6])); // 4
