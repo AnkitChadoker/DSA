48bb5fa4ed9307f347fae3e1583146ccb58af19d
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-04-07 20:12:06 +0530

    find Kth missing postive number, OPTIMAL

--- binary-search/BS-on-answers/Kth-missing-positive-number.js
+++ binary-search/BS-on-answers/Kth-missing-positive-number.js
@@ -32,14 +32,16 @@
  * 
  * WHY WE ARE DOING THIS & WHY IT IS WORKING ?:
  * 
- * we are just backing our statement (i), if k is less than arr[i], we can simply return it but if its not we would increment the k, by considering the present in between ele. as well.
+ * we are just backing our statement (i), if k is less than arr[i], we can simply return it but if its not we would increment the k, by considering the present in between ele. as well and return the k at the end.
 **/
 
 function kthMissingNumber(arr, k){
 	for(let i = 0; i < arr.length; i++){
 		if(arr[i] <= k) k++;
-		else return k;
+		else break;
 	}
+
+	return k;
 
 	/**
 	 * TC: O(n)
@@ -47,4 +49,87 @@ function kthMissingNumber(arr, k){
 	**/
 }
 
-console.log(kthMissingNumber([ 3, 5, 7, 10 ], 6)); // 9
+// console.log(kthMissingNumber([ 3, 5, 7, 10 ], 6)); // 9
+
+
+												/** OPTIMAL APPROACH **/
+
+/** since out brute is giving us the O(n) TC already and if we still need to optimize it, we can at max get to O(log n) TC, and we know we can only get this TC by binary search, but lets see if the constraints are ideal for binary search.
+ * 
+ * (i) do we have the sorted array ? YES
+ * (ii) do we need to find something in that array ? YES
+ * 			
+ * 		but the biggest fallback is binary search is used to find an element present in the array but our task is to find something which is not present in the array (k^th missing element).
+ *
+ * so we can apply binary search but we will not be able to find the exact result by just binary search because element is not present in the array, so how can we implement BS to get our answer ?? lets see, lets take and example
+ * 
+ * 	[3, 5, 7, 10], k = 6
+ * 
+ * we know the output is going to be 9, and 9 will be in between 7 and 10, so if some how we can get to this pair of element we can find our result (how ? will see later).
+ * 
+ * we are given that we need to find the 6^th element, and we know at each position of array we can get how many elements are missing till that element, how ??
+ *  at the 0^th index element 3 is there but ideally 1 should have been there and same for other elements as well, like below
+ * 	   
+ * 	   index  ->  [0, 1, 2, 3]	
+ *  	  arr ->  [3, 5, 7, 10]
+ *    missing ->  [2, 3, 4, 6]
+ * 
+ * so if at any position lets say on 0th index we want to know how many elements are missing we can simply do this
+ * 			arr[i] - (i + 1)
+ * 
+ * i = 0, 3 - (0 + 1) => 2
+ * i = 1, 5 - (1 + 1) => 3 etc.
+ * 
+ * so we can check for each mid if the missing number is smaller or greater on that mid position and based on the result we can eliminate the array.
+ * 
+ * and the pair of elements will get by the concept of opposite polarity where the high and low would cross each other those will be our elements between them we will find our missing number. (try dry run on given example)
+ * 
+ * we will get low = 3 and high = 2 (indices)
+ * 
+ * 									NOW, HOW TO GET THE MISSING ELEMENT ??
+ * 
+ * our high is at index 2, where missing count is 4 and we want the 6th missing number, so if till the high index we already have 4 missing count, and remaining 2 would be after that element only so we can just do 
+ * 
+ * 		arr[high] + (k - missing at high index);
+ * 
+ * 		7 + (6 - 4) => 9, which is what our output is.
+ * 
+ * 
+ * BUT, we can not directly take arr[high], because for the same example if k = 2 out high would be pointing to the -1 index and that would throw us error, so solve this situation we can solve the above equation and we will get the answer.
+ * 
+ * 		arr[high] + (k - missing at high index)
+ * 
+ * lets try to convert it into formula, we know how to find the missing elements at any index (arr[i] - (i + 1)).
+ * 		
+ * 	   arr[high] + (k - (arr[high] - (high + 1)));
+ *     
+ *     arr[high] + (k - (arr[high] - high - 1));
+ * 
+ *     arr[high] + (k - arr[high] + high + 1);
+ * 
+ *     k + high + 1 or k + low (formula)
+ * 
+ * becuase low is also pointing to the high + 1;
+ * 
+ *   6 + 2 + 1 => 9, which was expected, so we can use this formula instead of arr[high]
+**/
+
+
+function optimalKthMissingNumber(arr, k){
+	let low = 0;
+	let high = arr.length - 1;
+	while(low <= high){
+		const mid = Math.floor((low + high) / 2);
+		const missingCnt = arr[mid] - (mid + 1);
+		if(missingCnt < k){
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+
+	return k + low // k + high + 1;
+}
+
+
+console.log(optimalKthMissingNumber([ 3, 5, 7, 10 ], 6)); // 9
