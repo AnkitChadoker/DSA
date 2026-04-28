0e299da54a580b04c0ed712b6c5fa1e259bec75b
AnkitChadoker <ankitchadoker.dev@gmail.com>
2026-04-28 23:29:07 +0530

    sort character by frequency.

---
+++ strings/medium/sort-characters-by-frequency.js
@@ -0,0 +1,51 @@
+/**
+ * You are given a string s. Return the array of unique characters, sorted by highest to lowest occurring characters.
+ * 
+ * If two or more characters have same frequency then arrange them in alphabetic order.
+ *
+ * 
+ * Example 1
+ * Input : s = "tree"
+ * Output : ['e', 'r', 't' ] 
+ * Explanation :
+ * The occurrences of each character are as shown below :
+ * e --> 2
+ * r --> 1
+ * t --> 1.
+ * The r and t have same occurrences , so we arrange them by alphabetic order.
+ *
+ * 
+ * Example 2
+ * Input : s = "raaaajj"
+ * Output : ['a' , 'j', 'r' ]
+ * Explanation :
+ * The occurrences of each character are as shown below :
+ * a --> 4
+ * j --> 2
+ * r --> 1
+ * 
+**/
+
+
+/** INTUITION **/
+/** 
+ * we can first create a hashmap of characters containing the frequencies, once we have the complete hashing we can then sort the map based on the frequecies and if the frequecies matches then we can sort based on the character.
+**/
+
+function frequencySort(string){
+	const freqMap = {};
+
+	for (let char of string){
+		freqMap[char] = freqMap[char] ? freqMap[char] + 1 : 1;
+	}
+	// freqMap => { r: 1, a: 4, j: 2 }
+
+	const characters = Object.keys(freqMap);
+	// characters => [ 'r', 'a', 'j' ]
+
+	characters.sort( (a, b) => freqMap[a] !== freqMap[b] ? freqMap[b] - freqMap[a] : a.localeCompare(b));
+
+	return characters;
+}
+
+console.log(frequencySort("raaaajj")); //['a' , 'j', 'r' ]
